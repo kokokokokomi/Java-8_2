159268b2ca712e14a4654740737f65304a177de9
komisuomi <only-for-serzh@yandex.ru>
2020-10-14 16:08:34 +0300

    Added Lombok Plugin / Data Class Updated

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class Radio {
     private String name = "noname";
     private int maxVolume = 100;
@@ -15,92 +19,6 @@ public class Radio {
     private int pultStation;
     private boolean on;
 
-    public Radio() {
-    }
-
-    public Radio(String name, int maxVolume, int minVolume, int maxStationIndex, int minStationIndex, int currentStation, int currentVolume, int pultStation, boolean on) {
-        this.name = name;
-        this.maxVolume = maxVolume;
-        this.minVolume = minVolume;
-        this.maxStationIndex = maxStationIndex;
-        this.minStationIndex = minStationIndex;
-        this.currentStation = currentStation;
-        this.currentVolume = currentVolume;
-        this.pultStation = pultStation;
-        this.on = on;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getMaxStationIndex() {
-        return maxStationIndex;
-    }
-
-    public void setMaxStationIndex(int maxStationIndex) {
-        this.maxStationIndex = maxStationIndex;
-    }
-
-    public int getMinStationIndex() {
-        return minStationIndex;
-    }
-
-    public void setMinStationIndex(int minStationIndex) {
-        this.minStationIndex = minStationIndex;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        this.currentStation = currentStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
-
-    public int getPultStation() {
-        return pultStation;
-    }
-
-    public void setPultStation(int pultStation) {
-        this.pultStation = pultStation;
-    }
-
-    public boolean isOn() {
-        return on;
-    }
-
-    public void setOn(boolean on) {
-        this.on = on;
-    }
 
     public void nextStation() {
         int nextStation = getCurrentStation() +1;
